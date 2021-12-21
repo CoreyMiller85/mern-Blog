a3c7b0d09c4d22d11282a8fdd50a5aa50f4cbca0
Corey Miller <coreymiller454@gmail.com>
2021-12-20 21:51:39 -0500

    add authentication and routes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const { User } = require("./models/User");
 const bcrypt = require("bcrypt");
 const config = require("./config/key");
-
+const { auth } = require("./middleware/auth");
 const app = express();
 
 mongoose
@@ -18,6 +18,17 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.get("/api/user/auth", auth, (req, res) => {
+	res.status(200).json({
+		_id: req.user._id,
+		isAuth: true,
+		email: req.user.email,
+		name: req.user.name,
+		lastName: req.user.lastName,
+		role: req.user.role,
+	});
+});
 
 app.post("/api/users/register", (req, res) => {
 	const user = new User(req.body);
@@ -49,6 +60,13 @@ app.post("/api/user/login", (req, res) => {
 				res.cookie("x_auth", user.token).status(200).json({ success: true });
 			});
 		});
+	});
+});
+
+app.put("/api/user/logout", auth, (req, res) => {
+	User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+		if (err) return res.json({ success: false, err });
+		return res.status(200).json({ sucess: true });
 	});
 });
 
--- server/models/User.js
+++ server/models/User.js
@@ -70,6 +70,16 @@ userSchema.methods.generateToken = function (cb) {
 	});
 };
 
+userSchema.statics.findByToken = function (token, cb) {
+	const user = this;
+	jwt.verify(token, "secret", function (err, decode) {
+		User.findOne({ _id: decode, token: token }, function (err, user) {
+			if (err) return cb(err);
+			cb(null, user);
+		});
+	});
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
